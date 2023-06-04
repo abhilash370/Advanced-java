3a65ed8872c7c500b70c2305534185834966ccba
abhilash370 <91712069+abhilash370@users.noreply.github.com>
2023-06-04 11:13:54 +0530

    Add files via upload

---
+++ run.java
@@ -0,0 +1,7 @@
+public class run {
+    public static void main(String[] args) {
+        Q5 student = new Q5("John Doe", "123456", "Mathematics", 85, 100);
+        System.out.println(student.toString());
+    }
+}
+
